13f6c2543b722376513ec1d0a95f840093e28cb7
Luiz Henrique Souza <luiz.henrique@going2.com.br>
2023-04-23 12:18:42 -0400

    chore: eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
-import { Button, Input, Typography } from '@material-tailwind/react'
+import { Button, Input, Typography } from '@material-tailwind/react';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen w-11/12 max-w-xs items-center justify-center flex-col mx-auto space-y-4">
-
       <Typography variant="h4">Impostos do mês</Typography>
 
-     <Input label="Faturamento mensal" />
+      <Input label="Faturamento mensal" />
 
-     <Button className='w-full'>Calcular</Button>
+      <Button className="w-full">Calcular</Button>
     </main>
-  )
+  );
 }
